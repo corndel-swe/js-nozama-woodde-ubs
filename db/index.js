9e16e211fc75b3d800f1b0495357f9f209d72df5
Simon Haines <nimon@hotmail.co.uk>
2024-04-02 12:45:33 +0100

    adds d1e2 tests

--- db/index.js
+++ db/index.js
@@ -3,3 +3,15 @@ import { fileURLToPath } from 'url'
 
 // TODO: Follow the steps required to connect to the database using knex
 // Make sure your default export is a const called db
+
+const url = new URL('../db/db.sqlit', import.meta.url)
+
+const db = knex({
+  client: 'sqlite3',
+  connection: {
+    filename: fileURLToPath(url)
+  },
+  useNullAsDefault: true
+})
+
+export default db
